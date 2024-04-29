3b206ba1f26388dc6421ea484edc73f8c3e3661e
hvmegy <hamegy08lleo@gmail.com>
2024-04-29 17:07:57 +0700

    add date

--- script/script.js
+++ script/script.js
@@ -1,6 +1,27 @@
 function navigate2YearsWishes() { 
-    window.location.href = "pages/2YearsWishes.html"
+    window.location.href = "pages/2YearWishes/2YearsWishes.html"
 }
 function navigateGallery() { 
-    window.location.href ="pages/Gallery.html"
+    window.location.href ="pages/Gallery/Gallery.html"
 }
+
+function init() {
+    var ourDate = new Date("Apr 30, 2022 0:0:0").getTime();
+
+    var x = setInterval(function () {
+
+        var now = new Date().getTime();
+
+        var distance = now - ourDate;
+
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        document.getElementById("anniversary-counter").innerHTML = days + "d " + hours + "h "
+            + minutes + "m " + seconds + "s ";
+    }, 1000);
+}
+
+init()
